a6e526a78c0d014b6818e487c9bb424c4689a8db
Harald Vohringer <harald.voeh@gmail.com>
2024-05-31 09:56:12 +0200

    fixed implementation of plot only positve samples

--- src/plots.py
+++ src/plots.py
@@ -8,45 +8,36 @@ from bokeh.plotting import figure
 from config import App
 
 
-def bokeh_scatter(df, image=None):
-    if image is not None:
-        df = df[df.is_positive]
-    boolean_array = df.is_positive.values.astype(int)
-    color_array = np.array(["lightgrey", "red"])[boolean_array].tolist()
-    size_array = np.array([st.session_state.dotsize_neg, st.session_state.dotsize_pos])[
-        boolean_array
-    ].tolist()
-
+def bokeh_scatter(df, image, dotsize_pos, dotsize_neg,  positive):
     p = figure(match_aspect=True, sizing_mode="stretch_both")
 
-    if image is not None:
-        p.image(
-            image=[image[::-1, :]],
-            x=[0],
-            y=[0],
-            dw=[App.DEFAULT_SCALE * image.shape[1]],
-            dh=[App.DEFAULT_SCALE * image.shape[0]],
-        )
-        p.x_range.range_padding = 0
-        p.y_range.range_padding = 0
-        p.toolbar.active_scroll = p.select_one(WheelZoomTool)
+    p.image(
+        image=[image[::-1, :]],
+        x=[0],
+        y=[0],
+        dw=[App.DEFAULT_SCALE * image.shape[1]],
+        dh=[App.DEFAULT_SCALE * image.shape[0]],
+    )
+    p.x_range.range_padding = 0
+    p.y_range.range_padding = 0
+    p.toolbar.active_scroll = p.select_one(WheelZoomTool)
 
-    if image is not None:
+
+    p.circle(
+        df[df.is_positive]["X"],
+        df[df.is_positive]["Y"],
+        radius=dotsize_pos,
+        fill_color='lightgrey',
+        fill_alpha=0.1,
+        line_color="red",
+    )
+    if not positive:
         p.circle(
-            df["X"],
-            df["Y"],
-            radius=size_array,
-            fill_color=color_array,
+            df[~df.is_positive]["X"],
+            df[~df.is_positive]["Y"],
+            radius=dotsize_neg,
+            fill_color='lightgrey',
             fill_alpha=0.1,
-            line_color="red",
-        )
-    else:
-        p.circle(
-            df["X"],
-            df["Y"],
-            radius=size_array,
-            fill_color=color_array,
-            fill_alpha=1.0,
             line_color=None,
         )
 
