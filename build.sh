29db70dc1acb692a0fcf2a2b0010706086609630
Harald Vohringer <harald.voeh@gmail.com>
2024-06-05 10:10:00 +0200

    kubernetes stuff

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-$HOME/.docker/bin/docker build --platform linux/amd64 -t sagar87/binarazor:marc -f Dockerfile .
-$HOME/.docker/bin/docker push sagar87/binarazor:marc
+$HOME/.docker/bin/docker build --platform linux/amd64 -t sagar87/binarazor:leopard -f Dockerfile .
+$HOME/.docker/bin/docker push sagar87/binarazor:leopard
