7142da191c3a6a149155bbaf52bd436a4a3e83c1
Harald Vohringer <harald.voeh@gmail.com>
2024-12-05 10:18:16 +0100

    display polygon function

--- src/lasso.py
+++ src/lasso.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import plotly.express as px
-
+import plotly.graph_objects as go
 
 def plotly_lasso(mat, height):
     fig = px.imshow(mat, color_continuous_scale='gray')
@@ -8,5 +8,34 @@ def plotly_lasso(mat, height):
     fig.update_layout(coloraxis_showscale=False)
     fig.update_xaxes(showticklabels=False)
     fig.update_yaxes(showticklabels=False, scaleanchor="x", scaleratio=1)
-    
+    # config = {'scrollZoom': True}
+    # fig.show(config=config)
     return fig
+
+
+
+def plotly_polygons(mat, height, pts):
+    
+    # fig = go.Figure()
+    
+    fig = px.imshow(mat, color_continuous_scale='gray')
+    
+    fig.update_layout(width=height, height=height)
+    fig.update_layout(coloraxis_showscale=False)
+    fig.update_xaxes(showticklabels=False)
+    fig.update_yaxes(showticklabels=False, scaleanchor="x", scaleratio=1)
+    
+    
+    for r in pts:
+        x = [ item[0] for item in r['cells'] ] + [ r['cells'][0][0] ]
+        y = [ item[1] for item in r['cells'] ] + [ r['cells'][0][1] ]
+        fig.add_trace(
+            go.Scatter(
+                x=x,
+                y=y,
+                mode='lines',
+                name=r['status']
+            )
+        )
+    return fig
+        
